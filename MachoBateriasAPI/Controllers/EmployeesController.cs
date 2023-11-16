8e4626c0afc9d41220424dd9bd2dd71b5a54a732
Isaacfv2404 <jose.fallasvalenciano@ucr.ac.cr>
2023-11-15 22:50:38 -0600

    Employee Controller
    Se agregaron las utilidades para el empleado

--- MachoBateriasAPI/Controllers/EmployeesController.cs
+++ MachoBateriasAPI/Controllers/EmployeesController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MachoBateriasAPI.Data;
 using MachoBateriasAPI.Models;
@@ -12,7 +12,7 @@ namespace MachoBateriasAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeesController : Controller
+    public class EmployeesController : ControllerBase
     {
         private readonly MachoBateriasAPIContext _context;
 
@@ -20,15 +20,105 @@ namespace MachoBateriasAPI.Controllers
         {
             _context = context;
         }
+
         // GET: api/Employees
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Employee>>> GetSupplier()
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+        {
+          if (_context.Employee == null)
+          {
+              return NotFound();
+          }
+            return await _context.Employee.ToListAsync();
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+          if (_context.Employee == null)
+          {
+              return NotFound();
+          }
+            var employee = await _context.Employee.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+          if (_context.Employee == null)
+          {
+              return Problem("Entity set 'MachoBateriasAPIContext.Employee'  is null.");
+          }
+            _context.Employee.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.id }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
         {
             if (_context.Employee == null)
             {
                 return NotFound();
             }
-            return await _context.Employee.ToListAsync();
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employee.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return (_context.Employee?.Any(e => e.id == id)).GetValueOrDefault();
         }
     }
 }
--- MachoBateriasAPI/Models/Employee.cs
+++ MachoBateriasAPI/Models/Employee.cs
@@ -8,6 +8,7 @@
         public string address { get; set; }
         public string phone { get; set; }
         public string email { get; set; }
+        public string password { get; set; }
 
     }
 }
